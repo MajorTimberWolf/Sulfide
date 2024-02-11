806072bf58e3b1942353f9fb5ea5dcefa58c0b22
MajorTimberWolf <ak0929@srmist.edu.in>
2024-02-11 18:33:10 +0530

    feat: add error handling Added error handling which checks for the Usage Case

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
-int main(){
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        cerr << "Incorrect Usage" << endl;
+        cerr << "Proper Usage is: 'hydro <input.hy>'" << endl;
+        return EXIT_FAILURE;
+    }
+    cout << argv[1] << endl;
+    return EXIT_SUCCESS;
 }
