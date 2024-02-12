b621cee58f5f6a6035f834083574b13a9e42fe3e
MajorTimberWolf <ak0929@srmist.edu.in>
2024-02-12 13:52:01 +0530

    refactor: update header files

---
+++ cmake-build-relwithdebinfo/CMakeFiles/sulf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sulf.dir/src/main.cpp.o"
+  "CMakeFiles/sulf.dir/src/main.cpp.o.d"
+  "sulf"
+  "sulf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sulf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <optional>
 using namespace std;
+
+enum class TokenIdentifier{
+    _return,
+    _int_lit,
+    semi
+};
+struct Token{
+    TokenType iden;
+    optional<string> value;
+};
+
+
+
+
 int main(int argc, char* argv[]){
     if(argc != 2){
         cerr << "Incorrect Usage" << endl;
         cerr << "Proper Usage is: 'hydro <input.hy>'" << endl;
         return EXIT_FAILURE;
     }
-    cout << argv[1] << endl;
+    string contents;
+    {
+        stringstream contents_stream;
+        fstream input(argv[1], ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    cout << contents << endl;
     return EXIT_SUCCESS;
 }
